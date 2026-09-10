ec068104b9e25a05d7c96ae7c38d529ad6bfab9c
Arath <arath@ryu-ai.local>
2026-09-09 21:42:27 -0600

    Import pyVoIP.RTP as RTP in sip_telephony_service

--- sip_telephony_service.py
+++ sip_telephony_service.py
@@ -52,6 +52,7 @@ if sys.platform == "win32":
 import pyVoIP
 from pyVoIP.VoIP import VoIPPhone, PhoneStatus, CallState
 import pyVoIP.SIP as SIP
+import pyVoIP.RTP as RTP
 from voice_engine_ryu import RyuVoiceAgent
 from security_guard import (
     CallWatchdog,
